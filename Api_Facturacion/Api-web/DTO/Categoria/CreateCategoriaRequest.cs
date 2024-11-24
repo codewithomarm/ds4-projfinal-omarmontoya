d5ab759fe4dca6c6abbf2bd857826b82282084e4
codewithomarm <codewithomarm@gmail.com>
2024-11-24 14:50:05 -0500

    Agregando anotaciones para validaciones

--- Api_Facturacion/Api-web/DTO/Categoria/CreateCategoriaRequest.cs
+++ Api_Facturacion/Api-web/DTO/Categoria/CreateCategoriaRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,9 @@ namespace Api_web.DTO.Categoria
 {
     public class CreateCategoriaRequest
     {
+        [Required(ErrorMessage = "El nombre de la categoría es requerido")]
+        [StringLength(50, MinimumLength = 3,
+            ErrorMessage = "El nombre de la categoría debe tener entre 3 y 50 caracteres")]
+        public string Nombre { get; set; }
     }
 }
