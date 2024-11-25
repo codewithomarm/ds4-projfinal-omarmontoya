6c9b68b3b8ff12f6c25b7d48effd9710d83f3e4c
codewithomarm <codewithomarm@gmail.com>
2024-11-25 10:40:42 -0500

    Modificando configuracion del proyecto para arreglar error con metodo post

--- Api_Facturacion/Api-web/App_Start/WebApiConfig.cs
+++ Api_Facturacion/Api-web/App_Start/WebApiConfig.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Validation.Providers;
 
 namespace Api_web
 {
@@ -12,6 +14,13 @@ namespace Api_web
         {
             // Configuración y servicios de Web API
 
+            var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            formatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
             // Rutas de Web API
             config.MapHttpAttributeRoutes();
 
@@ -20,13 +29,6 @@ namespace Api_web
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-
-            // Configurar Web API para usar solo JSON
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            config.Formatters.Remove(config.Formatters.XmlFormatter);
-
-            // Ignorar referencias circulares en JSON
-            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
         }
     }
 }
