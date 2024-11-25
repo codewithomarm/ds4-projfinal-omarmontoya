1fc011f6f66ea32d5622f96f05338aee7c623664
codewithomarm <codewithomarm@gmail.com>
2024-11-25 18:31:09 -0500

    Agregando atributos para enviar respuesta al cliente desde el controlador

--- Api_Facturacion/Api-web/DTO/Usuario/UsuarioResponse.cs
+++ Api_Facturacion/Api-web/DTO/Usuario/UsuarioResponse.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Api_web.DTO;
+using Api_web.DTO.Rol;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,12 @@ namespace Api_web.DTO.Usuario
 {
     public class UsuarioResponse
     {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string NombreCompleto { get; set; }
+        public RolResponse Rol { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? UltimoAcceso { get; set; }
+        public string Estado { get; set; }
     }
 }
