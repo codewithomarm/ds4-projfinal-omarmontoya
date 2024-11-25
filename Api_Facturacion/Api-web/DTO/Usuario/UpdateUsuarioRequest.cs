1f8862ce9aa1bf328f438af522bd0fa6d8e7ed72
codewithomarm <codewithomarm@gmail.com>
2024-11-24 21:26:08 -0500

    Agregando atributos para dto usuario

--- Api_Facturacion/Api-web/DTO/Usuario/UpdateUsuarioRequest.cs
+++ Api_Facturacion/Api-web/DTO/Usuario/UpdateUsuarioRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,21 @@ namespace Api_web.DTO.Usuario
 {
     public class UpdateUsuarioRequest
     {
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre de usuario debe tener entre 3 y 50 caracteres")]
+        public string NombreUsuario { get; set; }
+
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 100 caracteres")]
+        public string Contrasena { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "El nombre completo no puede exceder los 100 caracteres")]
+        public string NombreCompleto { get; set; }
+
+        [Required]
+        public string Rol { get; set; }
+
+        [Required]
+        public string Estado { get; set; }
     }
 }
