a8e1706ec4e003563dd381e1e2d53c103031e289
codewithomarm <codewithomarm@gmail.com>
2024-11-25 15:08:49 -0500

    Agregando atributos para enviar respuestas desde el controlador de categorias

--- Api_Facturacion/Api-web/DTO/Subcategoria/SubcategoriaResponse.cs
+++ Api_Facturacion/Api-web/DTO/Subcategoria/SubcategoriaResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Api_web.DTO.Categoria;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,8 @@ namespace Api_web.DTO.Subcategoria
 {
     public class SubcategoriaResponse
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public CategoriaResponse Categoria { get; set; }
     }
 }
