03621c29ff0e547f880e5bfee5406abd7767f7a5
codewithomarm <codewithomarm@gmail.com>
2024-11-24 21:25:32 -0500

    Agregando atributos para dto Subcategoria

--- Api_Facturacion/Api-web/DTO/Subcategoria/UpdateSubcategoriaRequest.cs
+++ Api_Facturacion/Api-web/DTO/Subcategoria/UpdateSubcategoriaRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace Api_web.DTO.Subcategoria
 {
     public class UpdateSubcategoriaRequest
     {
+        [Required(ErrorMessage = "El nombre de la subcategoria es requerido")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre de la subcategoria debe tener entre 3 y 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la categoria es requerido")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre de la categoria debe tener entre 3 y 100 caracteres")]
+        public string Categoria { get; set; }
     }
 }
