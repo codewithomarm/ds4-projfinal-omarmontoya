299185de6842dc94cc57384c06e72eebc58cd930
codewithomarm <codewithomarm@gmail.com>
2024-11-26 16:01:56 -0500

    Agregando clases DTO para recibir y enviar respuestas desde el controlador de sucursal

---
+++ Api_Facturacion/Api-web/DTO/Sucursal/SucursalResponse.cs
@@ -0,0 +1,44 @@
+﻿using Api_web.DTO.Empresa;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Api_web.DTO.Sucursal
+{
+    public class SucursalResponse
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Provincia { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Distrito { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Corregimiento { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Urbanizacion { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Calle { get; set; }
+
+        [StringLength(50)]
+        public string Local { get; set; }
+
+        [Required]
+        public EmpresaResponse Empresa { get; set; }
+    }
+}
---
+++ Api_Facturacion/Api-web/DTO/Sucursal/UpdateSucursalRequest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Api_web.DTO.Sucursal
+{
+    public class UpdateSucursalRequest
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Provincia { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Distrito { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Corregimiento { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Urbanizacion { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Calle { get; set; }
+
+        [StringLength(50)]
+        public string Local { get; set; }
+
+        [Required]
+        public string Empresa { get; set; }
+    }
+}
